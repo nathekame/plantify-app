4285bcb776ca43f97f27bf78661f968bd27b1451
Norbert Osi Athekame <n.athekame@gmail.com>
2024-07-16 11:08:12 -0400

    integrated a landing page with links to all use cases and set up the docker configuration deleted one unwanted file

--- dev/app/.next/server/webpack-runtime.js
+++ dev/app/.next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("db6fa53863aea366")
+/******/ 		__webpack_require__.h = () => ("b4bae11090b9f33c")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- dev/app/.next/static/chunks/webpack.js
+++ dev/app/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e933e7fa161199c6"; }
+/******/ 		__webpack_require__.h = function() { return "4408765535819133"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
